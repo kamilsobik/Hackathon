e6f29ce36ed79c92e24e388a793983041f71dd0a
Kamil Sobik <kamil.sobik@gmail.com>
2022-08-27 16:58:20 +0200

    checkbox is ready

--- app/components/show-tasks/component.js
+++ app/components/show-tasks/component.js
@@ -1,9 +1,42 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class ProvidersOpenRequestComponent extends Component {
   @service store;
   @service session;
   @service router;
+  @tracked isDone = false;
+
+  // constructor() {
+  //   super(...arguments);
+  //   this.record = this.store.createRecord('task');
+  // }
+
+  // get hasEmptyField() {
+  //   const description = this.record;
+  //   return !description;
+  // }
+
+  // @action
+  // onPropertyChange(key, event) {
+  //   this.record[key] = event.target.value;
+  // }
+
+  // @action
+  // async onCreatNewTask() {
+  //   this.record.save();
+  // }
+
+  // @action
+  // async onDescriptionChange(title) {
+  //   await this.args.tasks.save();
+  // }
+
+  @action
+  async onIsDone(task) {
+    task.isDone = true;
+    await this.args.tasks.save();
+  }
 }
